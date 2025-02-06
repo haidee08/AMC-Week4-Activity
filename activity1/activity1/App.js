ae70dbe3b44f64ae065269b09738e7c0e603ea5d
De Guzman, Haidee M. <haideedeguzman463@gmail.com>
2025-02-06 16:33:40 +0800

    ScrollView

---
+++ activity1/activity1/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Text, ScrollView} from 'react-native';
+
+const App = () => (
+      <ScrollView>
+        <Text>
+         Song by
+Jaheim, Nelly
+
+I used to pride myself on being the other man
+But now it's flipped and I don't want you with no other man
+Why can't you understand anything I'm offering
+I gave you the world but you just wanted arguin
+From the time I picked you up
+Until the time I dropped you off again
+Even flipped out on me at the mall again
+It's his fault again' is what you telling all ya friends
+I ain't pointing fingers ma, I just wanna call again
+See how ya' doing, I know they stressin on ya
+I know them times going hard, thats why I'm checkin on ya'
+It's yours truly ma, I got a little message for ya'
+Anything he can do, girl, I can do it better for ya', 'cause
+
+When we laugh or we cry it's together
+Through the rain and the stormiest weather
+We gon' still be as one, it's forever
+It's forever
+
+Won't you come on and go with me ooh girl,
+Come on over to my place
+Won't you sit ya self down and take a sit
+And let me ease ya' mind girl
+We gon' do it our way
+
+I heard your friend told a friend that told a friend of mine
+That you was thinking that we should do it one more time
+If this ain't the truth, then hopefully it's not a lie
+'Cause I ain't got no issue's with hitting another time
+We never had a problem getting it done
+Disagreed upon a lot ma, but the sex wasn't one
+Now check it, I know you get excited (still)
+When I come round and bite (girl)
+Quit frowin' up and quit actin' like you don't like
+
+I like it, I like it, I really really like it,
+I want it, adore it, so come let me enjoy it
+
+When we laugh or we cry it's together
+Through the rain and the stormiest weather
+We gon' still be as one it's forever,
+It's forever
+
+Won't you come on and go with me ooh girl
+Come on over to my place
+Won't you sit ya' self down and take a seat
+And let me ease ya' mind girl
+We gon' do it our way
+
+Shawty where you been
+Feels like a long time, long, long-time since I seen ya
+Yes it has girl,
+When I know I said some fucked up things to you before
+But girl you know I didn't mean it
+(I didn't mean one single word,)
+(I never meant one single word)
+If I could take back every word, I would and more for sho'
+If I thougt that you believe it
+'Cause you make my life so convienent for me
+
+When we laugh or we cry, it's together
+Through the rain and the stormiest weather
+We gon' still be as one, it's forever
+It's forever
+
+Won't you come on and go with me ooh girl
+Come on over to my place
+Won't you sit ya self down and take a seat
+And let me ease ya mind girl
+We gon' do it our way
+
+I like it, I like it, I really really like it,
+I want it, adore it, so come let me enjoy it
+
+When we laugh or we cry, it's together
+Through the rain and the stormiest weather
+We gon' still be as one, it's forever
+It's forever
+
+        </Text>
+      </ScrollView>
+
+);
+
+export default App;
